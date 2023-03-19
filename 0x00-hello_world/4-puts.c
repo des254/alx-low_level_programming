6190c4403d868410a6ce2716d43db5c618a8a596
Desmond Maina <bdesmond254@gmail.com>
2023-03-19 13:02:26 -0700

    puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main() Use puts to print line of code
+ *
+ * Return: 0
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
