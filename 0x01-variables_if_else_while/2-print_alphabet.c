7ed7d7126a3ba7d26e9289b0801267c2fb0a4421
Desmond Maina <bdesmond254@gmail.com>
2023-10-10 13:11:02 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet in lowercase
+ * Return: o (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
