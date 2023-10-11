ce1dad7bf681ceac4f59b3bda07539d7f72220c6
Desmond Maina <bdesmond254@gmail.com>
2023-10-11 13:00:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar, followed by a new line
+ * Retur: 0 (Success)
+ */
+void myFunction()
+{
+	printf("_putchar\n");
+}
+
+int main()
+{
+	myFunction();
+	return (0);
+}
