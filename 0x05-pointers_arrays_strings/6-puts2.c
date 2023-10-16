da5ad561d4f8b52794e9fd37c1356ae88c779835
Desmond Maina <bdesmond254@gmail.com>
2023-10-16 09:34:11 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string
+ * @str: The string whose characters are printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
